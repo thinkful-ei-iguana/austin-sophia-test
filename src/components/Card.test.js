23fa7104c939f7723e2375931e90ca877750e095
Sophia Koeut <koeutsophia@gmail.com>
2019-11-12 13:02:28 -0800

    tests worked

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import Card from './Card';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Card from './Card';
+import store from './store'
+
+//import { exportAllDeclaration } from '@babel/types';
+
 
 
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Card />, div);
-    //ReactDOM.unmountComponenetAtNode(div);
+    ReactDOM.unmountComponentAtNode(div);
 }); 
+
+it('renders the UI as expected', () =>{
+
+    const tree = renderer
+      .create(<div class="Card"><h3>First card</h3><p>lorem ipsum</p><button>Delete</button></div>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
--- src/components/List.test.js
+++ src/components/List.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import List from './List';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
@@ -14,3 +15,10 @@ it('renders without crashing', () => {
     ReactDOM.unmountComponentAtNode(div);
 }); 
 
+it('renders the UI as expected', () =>{
+
+    const tree = renderer
+      .create(<section class="List"><header class="List-header">First list</header><div class="List-cards"><div class="Card"><h3>First card</h3><p>lorem ipsum</p><button>Delete</button></div><div class="Card"><h3>Second card</h3><p>lorem ipsum</p><button>Delete</button></div><div class="Card"><h3>Fifth card</h3><p>lorem ipsum</p><button>Delete</button></div><div class="Card"><h3>Sixth card</h3><p>lorem ipsum</p><button>Delete</button></div><div class="Card"><h3>Seventh card</h3><p>lorem ipsum</p><button>Delete</button></div><div class="Card"><h3>Tenth card</h3><p>lorem ipsum</p><button>Delete</button></div><div class="Card"><h3>Twelfth card</h3><p>lorem ipsum</p><button>Delete</button></div><div class="Card"><h3>Thirteenth card</h3><p>lorem ipsum</p><button>Delete</button></div></div><button class="List-add-button">+ Random Card</button></section>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+});
